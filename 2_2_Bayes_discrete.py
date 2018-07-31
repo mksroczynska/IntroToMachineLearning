19c2e3ba29531cd7804f8c33726b5fb42fb182e3
mksroczynska <mk.sroczynska@gmail.com>
2018-07-31 21:46:19 +0200

    Test functions with "pies.csv"

--- 2_2_Bayes_discrete.py
+++ 2_2_Bayes_discrete.py
@@ -16,3 +16,7 @@ def count_occurences(key, value, data):
         if row[key] == value:
             occurences += 1
     return occurences
+
+data = read_csv('pies.csv')
+number_of_circles = count_occurences('shape','circle', data)
+print(number_of_circles)
