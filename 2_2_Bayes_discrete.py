5d288f7223e1ae8a3a6632f1d688208964bb9684
mksroczynska <mk.sroczynska@gmail.com>
2018-07-31 21:44:28 +0200

    Add count_occurences

--- 2_2_Bayes_discrete.py
+++ 2_2_Bayes_discrete.py
@@ -8,3 +8,11 @@ def read_csv(filename):
         for row in reader:
             data.append(row)
     return data
+
+
+def count_occurences(key, value, data):
+    occurences = 0
+    for row in data:
+        if row[key] == value:
+            occurences += 1
+    return occurences
