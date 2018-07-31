07f337a2187eab4dfd0ea8bd0a4bd276512bff81
mksroczynska <mk.sroczynska@gmail.com>
2018-07-31 21:41:29 +0200

    Add read_csv function

---
+++ 2_2_Bayes_discrete.py
@@ -0,0 +1,10 @@
+import csv
+
+
+def read_csv(filename):
+    data = []
+    with open(filename, newline='') as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            data.append(row)
+    return data
