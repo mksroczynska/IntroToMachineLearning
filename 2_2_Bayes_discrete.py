bb5061fe4b5fa993e95a03e0d282093f25d0f45a
mksroczynska <mk.sroczynska@gmail.com>
2018-08-01 13:10:03 +0200

    Add joint_probability

--- 2_2_Bayes_discrete.py
+++ 2_2_Bayes_discrete.py
@@ -17,6 +17,18 @@ def count_occurences(key, value, data):
             occurences += 1
     return occurences
 
+
+def joint_probability(key1, value1, key_condition, value_condition, data):  # TODO change names of args
+    N = len(data)
+    occurences = 0
+    for row in data:
+        if row[key1] == value1 and row[key_condition] == value_condition:
+            occurences += 1
+    return occurences / N
+
+
 data = read_csv('pies.csv')
-number_of_circles = count_occurences('shape','circle', data)
-print(number_of_circles)
+print(len(data))
+number_of_circles = count_occurences('shape', 'circle', data)
+prob_circ_pos = joint_probability('shape', 'circle','class', 'pos', data)
+print(prob_circ_pos)
